b012586484b29d254b1f6fe7d1896a924d0d0066
Loan <110610727+okayhappex@users.noreply.github.com>
2025-11-04 09:51:31 +0100

    🎨 Changement dans la palette de couleurs

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
 				start_url: "/",
 				display: "standalone",
 				background_color: "#ffffff",
-				theme_color: '#ec003f',
+				theme_color: '#78a0dd',
 				icons: [
 					{ src: 'icons/icon-192x192.png', sizes: '192x192', type: 'image/png' },
 					{ src: 'icons/icon-512x512.png', sizes: '512x512', type: 'image/png' }
