91d4eee57b0a08d1d8e13d0510c2f24cb7665a35
Loan <loan.jean@ens.uvsq.fr>
2025-12-01 00:27:39 +0100

    :balloon: chore: Ajout d'un lien pour le SAE 104

--- src/views/RedirectHandler.vue
+++ src/views/RedirectHandler.vue
@@ -10,7 +10,8 @@
 		celcat: 'https://celcat.pages.dev',
 		planup: 'https://plan-up.pages.dev',
 		tuto_cpanel: 'https://youtu.be/iPS25YbKX-8',
-		tuto105: 'https://youtu.be/iPS25YbKX-8'
+		tuto105: 'https://youtu.be/iPS25YbKX-8',
+		sae104: 'https://cdn.discordapp.com/attachments/1443200806433063052/1443967398612041728/SAE_104_-_document_de_cadrage_2025-2026_v2.pdf?ex=692da1ef&is=692c506f&hm=28bf2c4d4137d4b3e8864960353b66490ded0127425a9f93875720b428af5d72'
 	}
 
 	onMounted(() => {
