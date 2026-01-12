5d465fbe49c6ca863c5247f5fb6c3c51f22013c6
Loan <happexx@proton.me>
2026-01-12 14:00:14 +0100

    Ajout du lien des docs du TP6

--- src/views/RedirectHandler.vue
+++ src/views/RedirectHandler.vue
@@ -7,12 +7,12 @@
 	const links = {
 		o2switch: 'https://servd162214.srv.odns.fr:2083/',
 		uvsq: 'https://www.uvsq.fr',
-		celcat: 'https://celcat.pages.dev',
-		planup: 'https://plan-up.pages.dev',
+		edt: 'https://vencat.mmi.codes',
+		planup: 'https://planup.mmi.codes',
 		tuto_cpanel: 'https://youtu.be/iPS25YbKX-8',
 		tuto105: 'https://youtu.be/iPS25YbKX-8',
 		sae104: 'https://drive.google.com/drive/folders/1CzdeX2eUZZaUQ0LTr84tA6D6KFcGiWaH',
-		sae104generique: '/docs/sae104_generique.pdf'
+		clech_tp6: 'https://drive.google.com/drive/folders/1FivQTRatw1VVvyMel8oFPC5imPhBGKnQ'
 	}
 
 	onMounted(() => {
